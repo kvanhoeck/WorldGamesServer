e6e94f2a937a4d0997e0adac9bd23136a6a54183
kvanhoeck <koen.vanhoeck@ae.be>
2014-11-24 14:08:30 +0100

    First Check In

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/api/flags', function (req, res) {
+    res.json({ "id": 2, "name": "Koen" })
+});
+
+module.exports = router;
