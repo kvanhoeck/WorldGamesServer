fe73329b71cba8075db24e81ae0c1e0057a09fd2
kvanhoeck <koen.vanhoeck@ae.be>
2014-11-27 12:10:23 +0100

    Header text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'World Games Server' });
 });
 
 router.get('/api/flags', function (req, res) {
