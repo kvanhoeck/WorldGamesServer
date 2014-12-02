84bfc83b9b961f43ccc1ca69b5bad42d00ef0055
kvanhoeck <koen.vanhoeck@ae.be>
2014-12-02 09:07:12 +0100

    MongoDB added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,50 @@ router.get('/api/flags', function (req, res) {
                ])
 });
 
+var databaseURL = "cognito:G6rzc4bal@ds055690.mongolab.com:55690/buytheworld";
+var collections = ["places"];
+var db = require("mongojs").connect(databaseURL, collections);
+
+app.get('/getAllPlaces', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    // The above 2 lines are required for Cross Domain Communication(Allowing the methods that come as Cross           // Domain Request
+    db.places.find('', function (err, places)
+    {
+    // Query in MongoDB via Mongo JS Module
+        if (err || !places) console.log("No places found");
+        else {
+            res.writeHead(200, { 'Content-Type': 'application/json' }); // Sending data via json
+            str = '[';
+            places.forEach(function (place) {
+                str = str + '{ "name" : "' + place.name + '"},' + '\n';
+            });
+            str = str.trim();
+            str = str.substring(0, str.length - 1);
+            str = str + ']';
+            res.end(str);
+                // Prepared the jSon Array here
+        }
+    });
+});
+
+app.post('/setPlace', function (req, res) {
+    console.log("POST: ");
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    // The above 2 lines are required for Cross Domain Communication(Allowing the methods that come as Cross 
+    // Domain Request
+    console.log(req.body);
+    console.log(req.body.mydata);
+    var jsonData = JSON.parse(req.body.mydata);
+    
+    db.places.save({ name: jsonData.name, lat: jsonData.geometry.location.lat, lng: jsonData.geometry.location.lng, icon: jsonData.icon, types: jsonData.types },
+       function (err, saved)
+    {
+ // Query in MongoDB via Mongo JS Module
+        if (err || !saved) res.end("Place not saved");
+        else res.end("Place saved");
+    });
+});
+
 module.exports = router;
