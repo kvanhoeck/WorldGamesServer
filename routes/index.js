72624d503e164f70f5fb3f1daa40b4826865ae14
kvanhoeck <koen.vanhoeck@ae.be>
2014-12-11 14:56:07 +0100

    'SendLatAndLng'

--- routes/index.js
+++ routes/index.js
@@ -251,7 +251,7 @@ router.post('/buyPlace', function (req, res) {
             console.log(req.body);
             
             var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
-            var place = db.getCollection("place").findOne({ "id": req.body.placeId });
+            var place = db.getCollection("place").findOne({ "geometry.location.lat": req.body.lat, "geometry.location.lng": req.body.lng });
             var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": jsonBody.userId, "placeId.$oid": jsonBody.placeId, "placeType": req.body.placeType });
             
             if (userPlace !== null)
