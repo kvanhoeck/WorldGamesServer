c48e2580088239c1008838cdf234d24dff65275e
kvanhoeck <koen.vanhoeck@ae.be>
2014-12-11 17:10:24 +0100

    'getPlace_Error'

--- routes/index.js
+++ routes/index.js
@@ -252,12 +252,12 @@ router.post('/buyPlace', function (req, res) {
             var place = db.getCollection("place").findOne({ "geometry.location.lat": req.body[0].lat, "geometry.location.lng": req.body[0].lng });
             var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": req.body[0].userId, "placeId": req.body[0].placeId, "placeType": req.body[0].placeType });
             
-            console.log("Searching for userId               : " + req.body[0].userId);
-            console.log("Searching for placeId              : " + req.body[0].placeId);
-            console.log("Searching for placeType            : " + req.body[0].placeType);
-            console.log("USERPLACE                          : " + userPlace);
-            console.log("USERPLACE (userPlace !== null)     :" + (userPlace !== null));
-            console.log("USERPLACE (userPlace !== undefined):" + (userPlace !== 'undefined'));;
+            console.log("Searching for userId              : " + req.body[0].userId);
+            console.log("Searching for placeId             : " + req.body[0].placeId);
+            console.log("Searching for placeType           : " + req.body[0].placeType);
+            console.log("USERPLACE                         : " + userPlace);
+            console.log("USERPLACE (userPlace == null)     :" + (userPlace == null));
+            console.log("USERPLACE (userPlace == undefined):" + (userPlace == 'undefined'));;
 
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
