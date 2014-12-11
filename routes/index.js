a11b6d92597afeb0c1ed714bb775e10238e3ffe6
kvanhoeck <koen.vanhoeck@ae.be>
2014-12-11 16:02:20 +0100

    'getPlace_Error'

--- routes/index.js
+++ routes/index.js
@@ -254,10 +254,13 @@ router.post('/buyPlace', function (req, res) {
             var place = db.getCollection("place").findOne({ "geometry.location.lat": jsonBody.lat, "geometry.location.lng": jsonBody.lng });
             var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": jsonBody.userId, "placeId.$oid": jsonBody.placeId, "placeType": req.body.placeType });
             
+            var place2 = db.getCollection("place").findOne({ "_id.$oid": '5489a00547b6640b009bf7fb' });
+
             console.log("USER        : " + user);
             console.log("USER.EMAIL  : " + user.email);
             console.log("USER.ID     : " + jsonBody.userId);
             console.log("PLACE       : " + place);
+            console.log("PLACE2.NAME : " + place2.name);
             console.log("Searching for " + req.body.lat + " , " + req.body.lng);
             console.log("Searching for " + jsonBody.lat + " , " + jsonBody.lng);
             console.log("Searching for " + req.body.place_id);
