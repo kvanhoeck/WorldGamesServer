b043ce4b3ad436e21c6d1aa8ea5af0ff2ce782ef
kvanhoeck <koen.vanhoeck@ae.be>
2014-12-17 11:40:21 +0100

    'RefacturingAllJSONRequests'

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,22 @@
 var router = express.Router();
 var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
-
+var Fiber = require('fibers');
+var MongoSync = require("mongo-sync");
+var BSON = require('mongodb').BSONPure;
+                
 function throwError(res, code, message, error) {
     console.log("ERROR: " + message);
     console.log("More info: " + error);
     res.status(code);
     res.send(message);
+}
+
+function getDB() {
+    var db = new MongoSync.Server().connect("mongodb://ds055690.mongolab.com:55690/buytheworld");
+    db.auth("cognito_btw", "G6rzc4dlr");
+    
+    return db;
 }
 
 /* GET home page. */
@@ -157,17 +167,14 @@ router.post('/buyPlace', function (req, res) {
     // The above 2 lines are required for Cross Domain Communication(Allowing the methods that come as Cross 
     // Domain Request
    
-    var Fiber = require('fibers');
-    var MongoSync = require("mongo-sync");
-    
     Fiber(function () {
         try {
             var db = getDB();
-            var jsonBody = JSON.stringify(req.body);
             
-            var user = db.getCollection("user").findOne({ "_id.$oid": req.body.userId });
-            var place = db.getCollection("place").findOne({ "geometry.location.lat": req.body[0].lat, "geometry.location.lng": req.body[0].lng });
-            var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": req.body.userId, "placeId": req.body[0].placeId, "placeType": req.body[0].placeType });
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
+            //old code: db.collection("userPlace").find({ "userId": new BSON.ObjectID(req.body.userId) }).toArray(function (err, places) {
+            var place = db.getCollection("place").findOne({ "geometry.location.lat": req.body.lat, "geometry.location.lng": req.body.lng });
+            var userPlace = db.getCollection("userPlace").findOne({ "userId": new BSON.ObjectID(req.body.userId), "placeId": req.body.placeId, "placeType": req.body.placeType });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
@@ -181,15 +188,14 @@ router.post('/buyPlace', function (req, res) {
                 if (wallet == null)
                     throwError(res, 400, "Could not retreive Wallet", "Wallet is null");
                 else {
-                    if (wallet.amount < req.body.price) throwError(res, 400, "You do not have enough money to buy this place", "To little in wallet: " + wallet.amount + " < " + req.body[0].price);
+                    if (wallet.amount < req.body.price) throwError(res, 400, "You do not have enough money to buy this place", "To little in wallet: " + wallet.amount + " < " + req.body.price);
                     else {
                         //Add place to user
-                        var BSON = require('mongodb').BSONPure;
                         var userPlace = [ {
                                 "userId": new BSON.ObjectID(user._id),
                                 "placeId": place.id,
-                                "placeType": req.body[0].placeType,
-                                "price": req.body[0].price,
+                                "placeType": req.body.placeType,
+                                "price": req.body.price,
                                 "name": place.name,
                                 "lat": place.geometry.location.lat,
                                 "lng": place.geometry.location.lng,
@@ -201,7 +207,7 @@ router.post('/buyPlace', function (req, res) {
                         db.getCollection('userPlace').insert(userPlace);
                         console.log("UserPlace well inserted");
                         //Substract price of wallet
-                        db.getCollection("wallet").update({ _id : wallet._id }, { userId: wallet.userId, amount: (wallet.amount - req.body[0].price) });
+                        db.getCollection("wallet").update({ _id : wallet._id }, { userId: wallet.userId, amount: (wallet.amount - req.body.price) });
                     }
                 }
             }
@@ -220,37 +226,19 @@ router.post('/getMyWorld', function (req, res) {
     // The above 2 lines are required for Cross Domain Communication(Allowing the methods that come as Cross 
     // Domain Request
     
-    var Fiber = require('fibers');
-    var MongoSync = require("mongo-sync");
-    
     Fiber(function () {
         try {
             var db = getDB();
-            //var jsonBody = JSON.stringify(req.body);
-            //var jsonBody2 = JSON.stringify(req.body[0]);
-            var mongo = require('mongodb');
-            var BSON = mongo.BSONPure;
             
-            console.log("GetMyWorld: Received: " + req.body.userId);
-            console.log("GetMyWorld: Received: " + req.body[0].userId);
-            console.log("GetMyWorld: Searching user '" + req.body.userId + "'");
-            //var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
-            //var user_ = db.getCollection("user").findOne({ "_id.$oid": jsonBody2.userId });
-            var user2 = db.getCollection("user").findOne({ "_id.$oid": req.body.userId });
-            var user4 = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
-            //console.log("GetMyWorld: User found: " + user.firstName + " '" + user._id.$iod + "'");
-            //console.log("GetMyWorld: User_ found: " + user_.firstName);
-            console.log("GetMyWorld: User2 found: " + user2.firstName);
-            console.log("GetMyWorld: User4 found: " + user4.firstName);
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
             //old code: db.collection("userPlace").find({ "userId": new BSON.ObjectID(req.body.userId) }).toArray(function (err, places) {
-            var userPlaces = db.getCollection("userPlace").find({ "userId.$oid": req.body.userId });
+            var userPlaces = db.getCollection("userPlace").find({ "userId": new BSON.ObjectID(req.body.userId) });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
             else if (userPlaces == null)
                 throwError(res, 400, "Could not retreive link between User and Place", "UserPlace is null");
             else {
-                console.log("GetMyWorld: User found: " + user.firstname);
                 var result = [];
                 userPlaces.forEach(function (up) {
                     console.log("GetMyWorld: Found " + up.name);
@@ -272,35 +260,50 @@ router.post('/getMyCapital', function (req, res) {
     // The above 2 lines are required for Cross Domain Communication(Allowing the methods that come as Cross 
     // Domain Request
     
-    MongoClient.connect("mongodb://ds055690.mongolab.com:55690/buytheworld", function (err, db) {
-        if (err) throwError(res, 400, "Could not connect to the database", err);
-        else {
-            console.log("Connected to MongoDB");
-            //Authenticate after connecting
-            db.authenticate('cognito_btw', 'G6rzc4dlr', function (authErr, success) {
-                if (authErr) throwError(res, 400, "Could not authenticate with the database", authErr);
-                else {
-                    console.log("Authenticated to MongoDB");
-                    
-                    //Check if the user exists
-                    var mongo = require('mongodb')
-                    var BSON = mongo.BSONPure;
-                    
-                    console.log("Searching for " + req.body.userId);
-                    
-                    db.collection("wallet").find({ "userId": new BSON.ObjectID(req.body.userId) }).toArray(function (err, wallet) {
-                        if (err) throwError(res, 400, "Could not retreive Wallet", err);
-                        else {
-                            console.log("retrieved records:");
-                            console.log(wallet);
-                            console.log("Sending this back to requester");
-                            res.json(wallet);
-                        }
-                    });
-                }
-            });
+    Fiber(function () {
+        try {
+            var db = getDB();
+            
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
+            var wallet = db.getCollection("wallet").findOne({ "userId": new BSON.ObjectID(req.body.userId) });
+
+            if (user == null)
+                throwError(res, 400, "Could not retreive User", "User is null");
+            else if (wallet == null)
+                throwError(res, 400, "Could not retreive Wallet", "Wallet is null");
+            else {
+                res.json(wallet);
+            }
+        } catch (e) {
+            console.log("ERROR: " + e);
+            throwError(res, 400, "Woops: " + e, e);
         }
-    });    
+    }).run();
+
+    //MongoClient.connect("mongodb://ds055690.mongolab.com:55690/buytheworld", function (err, db) {
+    //    if (err) throwError(res, 400, "Could not connect to the database", err);
+    //    else {
+    //        console.log("Connected to MongoDB");
+    //        //Authenticate after connecting
+    //        db.authenticate('cognito_btw', 'G6rzc4dlr', function (authErr, success) {
+    //            if (authErr) throwError(res, 400, "Could not authenticate with the database", authErr);
+    //            else {
+    //                console.log("Authenticated to MongoDB");
+    //                console.log("Searching for " + req.body.userId);
+                    
+    //                db.collection("wallet").find({ "userId": new BSON.ObjectID(req.body.userId) }).toArray(function (err, wallet) {
+    //                    if (err) throwError(res, 400, "Could not retreive Wallet", err);
+    //                    else {
+    //                        console.log("retrieved records:");
+    //                        console.log(wallet);
+    //                        console.log("Sending this back to requester");
+    //                        res.json(wallet);
+    //                    }
+    //                });
+    //            }
+    //        });
+    //    }
+    //});    
 });
 
 router.post('/cashPlace', function (req, res) {
@@ -316,11 +319,10 @@ router.post('/cashPlace', function (req, res) {
     Fiber(function () {
         try {
             var db = getDB();
-            var jsonBody = JSON.stringify(req.body);
             
-            var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
-            var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": req.body.userId, "placeId": req.body[0].placeId, "placeType": req.body[0].placeType });
-            var wallet = db.getCollection("wallet").findOne({ "userId.$oid": jsonBody.userId });
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
+            var userPlace = db.getCollection("userPlace").findOne({ "userId": new BSON.ObjectID(req.body.userId), "placeId": req.body.placeId, "placeType": req.body.placeType });
+            var wallet = db.getCollection("wallet").findOne({ "userId": new BSON.ObjectID(req.body.userId) });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
@@ -375,11 +377,10 @@ router.post('/checkInPlace', function (req, res) {
     Fiber(function () {
         try {
             var db = getDB();
-            var jsonBody = JSON.stringify(req.body);
             
-            var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
-            var userPlace = db.getCollection("userPlace").findOne({ "userId.$oid": req.body.userId, "placeId": req.body[0].placeId, "placeType": req.body[0].placeType });
-            var wallet = db.getCollection("wallet").findOne({ "userId.$oid": jsonBody.userId });
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
+            var userPlace = db.getCollection("userPlace").findOne({ "userId": new BSON.ObjectID(req.body.userId), "placeId": req.body.placeId, "placeType": req.body.placeType });
+            var wallet = db.getCollection("wallet").findOne({ "userId": new BSON.ObjectID(req.body.userId) });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
@@ -443,17 +444,16 @@ router.post('/resetWallet', function (req, res) {
     Fiber(function () {
         try {
             var db = getDB();
-            var jsonBody = JSON.stringify(req.body);
             
-            var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
-            var wallet = db.getCollection("wallet").findOne({ "userId.$oid": jsonBody.userId });
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
+            var wallet = db.getCollection("wallet").findOne({ "userId": new BSON.ObjectID(req.body.userId) });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
             else if (wallet == null)
                 throwError(res, 400, "Could not retreive Wallet", "Wallet is null");
             else {
-                db.getCollection("wallet").update({ _id : wallet._id }, { userId: wallet.userId, amount: parseInt(req.body[0].resetValue) });
+                db.getCollection("wallet").update({ _id : wallet._id }, { userId: wallet.userId, amount: parseInt(req.body.resetValue) });
                 
                 res.json([{ "Code": "SAVED" }]);
             }
@@ -478,26 +478,22 @@ router.post('/findMyPlacesNearby', function (req, res) {
     Fiber(function () {
         try {
             var db = getDB();
-            var jsonBody = JSON.stringify(req.body);
             
-            var user = db.getCollection("user").findOne({ "_id.$oid": jsonBody.userId });
+            var user = db.getCollection("user").findOne({ "_id": new BSON.ObjectID(req.body.userId) });
             
             if (user == null)
                 throwError(res, 400, "Could not retreive User", "User is null");
             else {
-                var mongo = require('mongodb')
-                var BSON = mongo.BSONPure;
-                
                 //console.log("FindMyPlacesNearby: Received JSON " + jsonBody);
-                console.log("FindMyPlacesNearby: Searching places between " + (parseFloat(req.body[0].lat) - 0.01) + " and " + (parseFloat(req.body[0].lat) + 0.01));
-                console.log("FindMyPlacesNearby: Searching places between " + (parseFloat(req.body[0].lng) - 0.01) + " and " + (parseFloat(req.body[0].lng) + 0.01));
+                console.log("FindMyPlacesNearby: Searching places between " + (parseFloat(req.body.lat) - 0.01) + " and " + (parseFloat(req.body.lat) + 0.01));
+                console.log("FindMyPlacesNearby: Searching places between " + (parseFloat(req.body.lng) - 0.01) + " and " + (parseFloat(req.body.lng) + 0.01));
                 var myLocations = db.getCollection("userPlace").find(
                     {
                         "$and": [   { "userId.$oid": req.body.userId },
-                                    { "lat": { "$gt": (parseFloat(req.body[0].lat) - 0.01) } }, 
-                                    { "lat": { "$lt": (parseFloat(req.body[0].lat) + 0.01) } },
-                                    { "lng": { "$gt": (parseFloat(req.body[0].lng) - 0.01) } },
-                                    { "lng": { "$lt": (parseFloat(req.body[0].lng) + 0.01) } }
+                                    { "lat": { "$gt": (parseFloat(req.body.lat) - 0.01) } }, 
+                                    { "lat": { "$lt": (parseFloat(req.body.lat) + 0.01) } },
+                                    { "lng": { "$gt": (parseFloat(req.body.lng) - 0.01) } },
+                                    { "lng": { "$lt": (parseFloat(req.body.lng) + 0.01) } }
                         ]
                     });
                 var result = [];
@@ -536,15 +532,5 @@ router.post('/getPrice', function (req, res) {
         }
     });
 });
-
-function getDB() {
-    var Fiber = require('fibers');
-    var MongoSync = require("mongo-sync");
-
-    var db = new MongoSync.Server().connect("mongodb://ds055690.mongolab.com:55690/buytheworld");
-    db.auth("cognito_btw", "G6rzc4dlr");
-
-    return db;
-}
 
 module.exports = router;
